b52c9b08f237d5dab8a035731e00b2451b3d81ca
astev89 <aaron.stevens@randrr.com>
2018-02-06 22:32:05 -0500

    Add folders for other views

--- src/components/FalconNav/index.js
+++ src/components/FalconNav/index.js
@@ -3,14 +3,19 @@ import './_falcon-nav.scss'
 
 export default function FalconNav(props) {
   return [
-    <nav className="falconNav">
-      <ul className="falconNav__container">
-        <li>Home</li>
-        <li>Team</li>
-        <li>Our Promise</li>
-        <li>FAQ</li>
-      </ul>
-    </nav>,
+    <header key="header" className="falcon-header">
+      <h1 className="falcon-header__heading">Falcon Consulting</h1>
+      <nav className="falcon-nav">
+        <ul className="falcon-nav__container">
+          <li><a>Home</a></li>
+          <li><a>Team</a></li>
+          <li><a>Our Promise</a></li>
+          <li><a>FAQ</a></li>
+        </ul>
+      </nav>
+      <h2 className="falcon-header__heading-2">Let Your Investments Soar</h2>
+      <p className="falcon-header__subtext">Your all inclusive crypto currency mining solution.</p>
+    </header>,
     props.children
   ]
 }
