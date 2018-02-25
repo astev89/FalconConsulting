1ba2f6104a63354052e92e010c9ede8ea3bf255d
astev89 <aaron.stevens@randrr.com>
2018-02-25 00:52:05 -0500

    Some copy changes and removing trailing spaces

--- src/main/Faq/index.js
+++ src/main/Faq/index.js
@@ -26,31 +26,31 @@ export default class Faq extends Component {
             Why invest now?
           </h3>
           <p className="faq-block__text">
-            The sooner you invest the sooner you can start mining and generating crypto! 
+            The sooner you invest the sooner you can start mining and generating crypto!
             This means you get more for you buck in terms of difficulty level and current market value of the coin
             being mined. Early Investors also have smaller investment requirements.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="faq-block">
           <h3 className="faq-block__heading">
-            What is difficulty?
+            What is difficulty in regards to mining?
           </h3>
           <p className="faq-block__text">
             Difficulty is the measure of how challenging it is to generate a solution to the mining "puzzle" that will in turn generate a
             coin once solved.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="faq-block">
           <h3 className="faq-block__heading">
             What if I don't have enough funds?
           </h3>
           <p className="faq-block__text">
-            Don't have the funds, but know someone who might? We offer a very rewarding referral program for any investor you send our way! 
+            Don't have the funds, but know someone who might? We offer a very rewarding referral program for any investor you send our way!
             These referral fees start at 2% and cap at 5% and are sent every payout your referral gets paid!
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="faq-block">
           <h3 className="faq-block__heading">
@@ -58,26 +58,27 @@ export default class Faq extends Component {
           </h3>
           <p className="faq-block__text">
             We are referring to 2 to 5 percent of all of the crypto mined from Falcon Consulting's percentage of the contract.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="faq-block">
           <h3 className="faq-block__heading">
-            How often do i get paid?
+            How often do I get paid?
           </h3>
           <p className="faq-block__text">
             All payments are sent directly to your wallet of choice every 2nd and 4th Friday of the month.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="faq-block">
           <h3 className="faq-block__heading">
-            Where do i set up my wallet to receive payment?
+            Where do I set up my wallet to receive payment?
           </h3>
           <p className="faq-block__text">
-            We recommend our customers to set up a wallet on GDAX.com. If you are uncomfortable with setting up your wallet our Team will be more than happy to walk you through the process.
-          </p> 
-        </div> 
+            We recommend our customers to set up a wallet on <a href="http://www.gdax.com" target="_blank">GDAX.com</a>.
+            If you are uncomfortable with setting up your wallet our Team will be more than happy to walk you through the process.
+          </p>
+        </div>
 
          <div className="faq-block">
           <h3 className="faq-block__heading">
@@ -85,8 +86,8 @@ export default class Faq extends Component {
           </h3>
           <p className="faq-block__text">
             No, currently at this time we do not accept credit cards. All funds must be wire transferred Campione Law P.A. escrow account.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
           <div className="faq-block">
           <h3 className="faq-block__heading">
@@ -94,38 +95,38 @@ export default class Faq extends Component {
           </h3>
           <p className="faq-block__text">
             The level of mining difficulty relative to the coin being mined, hashes per second, and a little bit of luck.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
           <div className="faq-block">
           <h3 className="faq-block__heading">
             What is BlockChain?
           </h3>
           <p className="faq-block__text">
-            A Blockchain is a log of transactions or data that is shared and verified by everyone participating in the network. Simply a 
-            distributed database.
-          </p> 
-        </div> 
+            A Blockchain is a log of transactions or data that is shared and verified by everyone participating in the network.
+            Simply put, it's a distributed database.
+          </p>
+        </div>
 
             <div className="faq-block">
           <h3 className="faq-block__heading">
             What are crypto currencies?
           </h3>
           <p className="faq-block__text">
-            Crypto currencies are digital currencies that vary in functionality in which encryption techniques are used to control the generation 
+            Crypto currencies are digital currencies that vary in functionality in which encryption techniques are used to control the generation
             of coins. This new form of currency operates independently from any trust or central banking system.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="faq-block">
           <h3 className="faq-block__heading">
             What is difficulty level?
           </h3>
           <p className="faq-block__text">
-            Difficulty is a measurement of how difficult it is to find a hash or solution below a given target. Difficulty level is ever changing and can 
+            Difficulty is a measurement of how difficult it is to find a hash or solution below a given target. Difficulty level is ever changing and can
             increase or decrease over time.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
          <div className="faq-block">
           <h3 className="faq-block__heading">
@@ -134,9 +135,9 @@ export default class Faq extends Component {
           <p className="faq-block__text">
             Mining is the process of collecting transaction data, verifying transactions, putting that data in a block and appending it to the block
             chain. Simply, mining is solving a very long math problem.
-          </p> 
-        </div> 
-        
+          </p>
+        </div>
+
       </main>
     )
   }
--- src/main/Offer/index.js
+++ src/main/Offer/index.js
@@ -18,81 +18,81 @@ export default class Offer extends Component {
             Our Offer
           </h2>
           <p className="falcon-header__subtext">
-            Why you should invest with Falcon Consulting 
+            Why you should invest with Falcon Consulting
           </p>
         </PortalComponent>
 
         <div className="offer-block">
           <h3 className="offer-block__heading">
             Our Unique Approach
-          </h3> 
+          </h3>
           <hr/>
           <p className="offer-block__text">
-            At Falcon Consulting our approach to investing in mining is different from traditional tactics. Other cloud mining solutions want you to 
-            rent the hardware that you never get to see, and have to base so much of the decision on blind trust. At Falcon Consulting we use your investment 
-            to purchase hardware that belongs to YOU! 
+            At Falcon Consulting our approach to investing in mining is different from traditional tactics. Other cloud mining solutions want you to
+            rent hardware that you never get to see, and base so much of your decision on blind trust. At Falcon Consulting we use your investment
+            to purchase hardware that belongs to YOU!
 
-            We offer a generous buy back program, in purchasing the hardware back from you. First round investors will recieve 75% of 
-            their investment returned back to them buy the end of their contract. We have chosen this business plan to better insure a safer investment 
+            Furthermore, we offer a generous buy back program to purchase the hardware back from you. First round investors will recieve 75% of
+            their investment returned back to them by the end of their contract. We have chosen this business plan to better insure a safer investment
             for our investors.
-            
-            {/*This inludes solutions per second, temperatures, amount you have mined, etc. In addition to that, we cover all of your maintenance
-            and repair costs at no additional expense to you and even offer a buy back program on your purchased hardware to help you hit your R.O.I even
-            faster! */}
-          </p> 
-        </div> 
+          </p>
+        </div>
 
 
         <div className="offer-block">
           <h3 className="offer-block__heading">
-            Your Mining Profitabliity  
-          </h3> 
+            Your Mining Profitabliity
+          </h3>
           <hr/>
           <p className="offer-block__text">
-          We are 100% committed to delivering a VERY profitable investment to all of our customers. The hardware you purchase through us is absolutely 
-          top of the line which ensures consistency for the duration of your contract. All of the details regarding the hardware are presented to our 
-          investors upon meeting with our team.
+            We are 100% committed to delivering a VERY profitable investment to all of our customers. The hardware you purchase through us is absolutely
+            top of the line which ensures consistency for the duration of your contract. All of the details regarding the hardware are presented to our
+            investors upon meeting with our team.
+          </p>
+          <p>HERE is OUR ESTIMATED HASHING POWER PER PLAN:</p>
+          <ul>
+            <li> Tier 1 $15,000
+              700 MH/s
+            </li>
+            <li> Tier 2 $100,000
+              5,700 MH/s
+            </li>
+            <li> Tier 3 $250,000
+              14,000 MH/s
+            </li>
+          </ul>
+          <p>
+            we always encourage our customers to do their own research and use &nbsp;
+            <a href="https://www.cryptocompare.com/mining/calculator/eth" target="_blank">
+              mining profitability calculators to determine ROI
+            </a>
+          </p>
 
-          HERE is OUR ESTIMATED HASHING POWER PER PLAN:
-            <ul> 
-              <li> Tier 1 $15,000
-                700 MH/s
-              </li>
-              <li> Tier 2 $100,000
-                5,700 MH/s
-              </li>
-              <li> Tier 3 $250,000
-                14,000 MH/s
-              </li>
-            </ul>
-          we always encourage our customers to do their own research and use <a href="https://www.cryptocompare.com/mining/calculator/eth"> mining profitability calculators to determine ROI </a>
-          </p> 
+        </div>
 
-        </div> 
-       
         <div className="offer-block">
           <h3 className="offer-block__heading">
             Mining With Us Is Easy
-          </h3> 
+          </h3>
           <hr/>
           <p className="offer-block__text">
-          When you enter into a contract with us you can forget about all the headaches that come with setting up and maintaining your hardware; we 
-          take care of everything for you. No worrying about loud, hot machines running up your energy bill or learning the complexities of mining 
+          When you enter into a contract with us you can forget about all the headaches that come with setting up and maintaining your hardware; we
+          take care of everything for you. No worrying about loud, hot machines running up your energy bill or learning the complexities of mining
           software. Falcon Consulting has you covered!
-          </p> 
-        </div> 
+          </p>
+        </div>
 
 
         <div className="offer-block">
           <h3 className="offer-block__heading">
             We Support Altcoin Mining
-          </h3> 
+          </h3>
           <hr/>
           <p className="offer-block__text">
             There is immense value associated with the altcoin cryptocurrency market. As such, we understand that it is likely that our customers
             will want to get involved. That is why we offer a means to pivot bewteen the most profitable currencies to maximize profitablity.
-          </p> 
-        </div> 
+          </p>
+        </div>
 
         <div className="offer-block">
           <h3 className="offer-block__heading">
@@ -100,13 +100,13 @@ export default class Offer extends Component {
           </h3>
           <hr/>
           <p className="offer-block__text">
-            Most of our initial investors have worked with one of our tem members in the past and have trusted our expertise to give them results. 
-            This same foundation of trust is the pillar of Falcon Consulting. 
-            We value each and every one of our investors and stay 100% committed to complete transparency with them. 
-            If you feel that you need further explanation of mining or blockchain technology, 
-            the impressive value attached to it or how it will revolutionize the future, 
+            Most of our initial investors have worked with one of our tem members in the past and have trusted our expertise to give them results.
+            This same foundation of trust is the pillar of Falcon Consulting.
+            We value each and every one of our investors and stay 100% committed to complete transparency with them.
+            If you feel that you need further explanation of mining or blockchain technology,
+            the impressive value attached to it or how it will revolutionize the future,
             our team will be more than happy to consult you in person or virtually through Google Hangouts.
-          </p> 
+          </p>
         </div>
 
       </main>
